5e160e59ae107d63c8030751a36b2fdb664d9d4f
Luis C. Corrales <lc.corrales01@gmail.com>
2016-11-08 14:42:22 -0600

    Issue #22 :: adding test

---
+++ test/page-manager-test.js
@@ -0,0 +1,23 @@
+/**                   _
+ *  _             _ _| |_
+ * | |           | |_   _|
+ * | |___  _   _ | | |_|
+ * | '_  \| | | || | | |
+ * | | | || |_| || | | |
+ * |_| |_|\___,_||_| |_|
+ *
+ * (c) Huli Inc
+ */
+
+/* global expect, describe, it */
+
+define([
+        'ju-mvc/page-manager',
+        'testing-helper/routes'
+    ],
+    function(
+        PageManager,
+        routes
+    ) {
+
+});
